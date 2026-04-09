d658d3c13ef05bd4c93a44b5ba4e3e7a0acc2982
vipulpatodi <vipul.patodi@nagarro.com>
2026-04-09 10:33:38 +0000

    change the API to support dev and prod

--- frontend/ui-app/src/services/api.ts
+++ frontend/ui-app/src/services/api.ts
@@ -1,8 +1,8 @@
-// const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export async function getProducts() {
-  const res = await fetch(`/api/products`);
-  // const res = await fetch(`${API_BASE}/api/products`);
+  // const res = await fetch(`/api/products`);
+  const res = await fetch(`${API_BASE}/api/products`);
 
   return res.json();
 }
