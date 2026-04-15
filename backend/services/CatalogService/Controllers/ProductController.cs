23667585a3f9f8c08eff21f90a2a98029332725d
vipulpatodi <vipul.patodi@nagarro.com>
2026-04-15 20:34:51 +0000

    changed url

--- backend/services/CatalogService/Controllers/ProductController.cs
+++ backend/services/CatalogService/Controllers/ProductController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 namespace CatalogService.Controllers
 {
 
-    //[Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
